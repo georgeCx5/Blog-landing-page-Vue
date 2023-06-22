1582b3bb2dff79d4a7cb9f9fd676ca8c0994eaff
georgeCx5 <69874793+georgeCx5@users.noreply.github.com>
2023-06-22 02:11:23 -0500

    Add files via upload

--- App.vue
+++ App.vue
@@ -1,5 +1,6 @@
 <script>
 import HeaderButtons from './components/HeaderButtons.vue';
+import NavBarUpperBtn from './components/NavBarUpperBtn.vue';
 
 import logo from '@/assets/images/logo.svg?url';
 import IconHamburger from '@/assets/images/icon-hamburger.svg?component';
@@ -13,6 +14,7 @@ export default {
   },
   components: {
     HeaderButtons,
+    NavBarUpperBtn,
 
     IconHamburger,
     IconClose,
@@ -23,11 +25,28 @@ export default {
   <body class=" flex flex-col items-center font-overpass">
     <header
       class=" flex flex-col gap-[108px] w-full max-w-[425px] pt-14 pb-[156px] bg-gradient-to-br from-neo-light-red to-neo-very-dark-blue text-neo-white rounded-es-[100px]">
-      <nav class=" flex justify-between items-center px-6">
+      <nav class=" relative flex justify-between items-center px-6">
         <img class=" h-8 select-none" :src="logo" alt="logo" draggable="false">
-        <div @click="isBarOpen=!isBarOpen" class=" cursor-pointer">
+        <div @click="isBarOpen = !isBarOpen" class=" cursor-pointer select-none">
           <IconHamburger v-show="!isBarOpen" />
           <IconClose v-show="isBarOpen" />
+        </div>
+        <div v-show="isBarOpen"
+          class=" absolute flex flex-col gap-6 right-6 top-16 w-[85%] pt-6 pb-8 bg-neo-white rounded-[5px] drop-shadow-nav">
+          <NavBarUpperBtn btn-text="Product" :data-index="0" />
+          <NavBarUpperBtn btn-text="Company" :data-index="1" />
+          <NavBarUpperBtn btn-text="Connect" :data-index="2" />
+          <div class=" flex flex-col items-center gap-6">
+            <div class=" w-[85%] h-[1px] bg-neo-nav-line"></div>
+            <button
+              class=" text-neo-very-dark-blue hover:text-neo-very-light-red text-[18px] leading-[28px] font-semibold rounded-[28px]">
+              Login
+            </button>
+            <button
+              class=" w-[137px] h-[48px] bg-gradient-to-br from-neo-very-light-red-two to-neo-light-red-two hover:bg-none hover:bg-neo-very-light-red text-[16px] leading-[18px] font-bold rounded-[28px]">
+              Sign Up
+            </button>
+          </div>
         </div>
       </nav>
       <div class=" flex flex-col items-center gap-12 px-6">
---
+++ components/NavBarUpperBtn.vue
@@ -0,0 +1,67 @@
+<script>
+import ArrowOrange from '@/assets/images/icon-arrow-dark.svg?component';
+
+export default {
+   data() {
+      return {
+         isOpened: false,
+         data: [
+            [
+               'Overview',
+               'Pricing',
+               'Marketplace',
+               'Features',
+               'Integrations'
+            ],
+            [
+               'About',
+               'Team',
+               'Blog',
+               'Careers'
+            ],
+            [
+               'Contact',
+               'Newsletter',
+               'LinkedIn'
+            ]
+         ],
+      }
+   },
+   props: {
+      btnText: String,
+      dataIndex: {
+         Number,
+         default: 0,
+      }
+   },
+   components: {
+      ArrowOrange,
+   },
+   computed: {
+      arrowState() {
+         let opacity;
+         let rotate;
+         if (this.isOpened) {
+            rotate = 'rotate-180';
+            opacity = 'text-opacity-75';
+         }
+         return { rotate, opacity }
+      }
+   }
+}
+</script>
+<template>
+   <div
+      :class="` flex flex-col gap-6 items-center text-neo-very-dark-blue ${arrowState.opacity} text-[18px] leading-[28px] font-semibold`">
+      <button @click="isOpened = !isOpened" class=" flex items-center gap-2 hover:text-neo-very-light-red">
+         {{ btnText }}
+         <ArrowOrange :class="`${arrowState.rotate}`" />
+      </button>
+      <div v-show="isOpened"
+         :class="` flex flex-col gap-3 w-[85%] pt-4 pb-6 bg-neo-very-dark-gray-blue bg-opacity-[.08] rounded-[5px]`">
+         <button v-for="element in data[dataIndex]" class=" text-[16px] hover:text-neo-very-light-red">
+            {{ element }}
+         </button>
+      </div>
+   </div>
+</template>
